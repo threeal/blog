5d47824462d9e556d4f6de9bcc35d5389d96eeae
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-03 20:22:46 +0700

    style: replace tabs with spaces

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,12 @@
 module.exports = {
-	printWidth: 100,
-	tabWidth: 2,
-	useTabs: true,
-	plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
-	overrides: [
-		{
-			files: "*.astro",
-			options: {
-				parser: "astro",
-			},
-		},
-	],
+  printWidth: 100,
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
 };
