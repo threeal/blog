9d2cbd307abf41816419fa4ef043ab412855e001
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-03 20:09:11 +0700

    style: simplify `.prettierrc.js` config file

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,8 @@
-/** @type {import("@types/prettier").Options} */
 module.exports = {
 	printWidth: 100,
-	semi: true,
-	singleQuote: false,
 	tabWidth: 2,
 	useTabs: true,
-	plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss" /* Must come last */],
+	plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
 	overrides: [
 		{
 			files: "*.astro",
